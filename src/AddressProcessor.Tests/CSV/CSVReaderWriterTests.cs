d94f96053b4572e7cfb348f60bfb2c28925684b7
lironschur <lironschur@gmail.com>
2016-12-03 22:45:16 +0000

    First excercise
    Code cleanup, added basic UTs

---
+++ src/AddressProcessor.Tests/CSV/CSVReaderWriterTests.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using AddressProcessing.CSV;
+
+namespace Csv.Tests
+{
+	[TestFixture]
+	public class CSVReaderWriterTests
+	{
+		private const string testFile = "test1.csv";
+
+		[Test]
+		public void CSVReaderWriter_OpenRead_Success()
+		{
+			// Arrange - create file using writer
+			using (var csvReaderWriter = new CSVReaderWriter())
+			{
+				csvReaderWriter.Open(testFile, CSVReaderWriter.Mode.Write);
+			}
+
+			// Act Assert
+			using (var csvReaderWriter = new CSVReaderWriter())
+			{
+				csvReaderWriter.Open(testFile, CSVReaderWriter.Mode.Read);
+			}
+		}
+
+		[Test]
+		public void CSVReaderWriter_OpenWrite_Success()
+		{
+			// Act Assert
+			using (var csvReaderWriter = new CSVReaderWriter())
+			{
+				csvReaderWriter.Open(testFile, CSVReaderWriter.Mode.Write);
+			}
+		}
+
+		[Test]
+		[ExpectedException(typeof(ArgumentOutOfRangeException))]
+		public void CSVReaderWriter_OpenInvalidMode_ThrowsArgumentOutOfRangeException()
+		{
+			// Act Assert
+			using (var csvReaderWriter = new CSVReaderWriter())
+			{
+				csvReaderWriter.Open(testFile, (CSVReaderWriter.Mode)1234);
+			}
+		}
+
+		[Test]
+		public void CSVReaderWriter_WriteThenRead_Success()
+		{
+			const string VAL1 = "val1";
+			const string VAL2 = "val2";
+
+			using (var csvReaderWriter = new CSVReaderWriter())
+			{
+				csvReaderWriter.Open(testFile, CSVReaderWriter.Mode.Write);
+				csvReaderWriter.Write(VAL1, VAL2);
+			}
+
+			using (var csvReaderWriter = new CSVReaderWriter())
+			{
+
+				string column1 = null, column2 = null;
+
+				csvReaderWriter.Open(testFile, CSVReaderWriter.Mode.Read);
+				var result = csvReaderWriter.Read(out column1, out column2);
+				Assert.AreEqual(true, result);
+				Assert.AreEqual(VAL1, column1);
+				Assert.AreEqual(VAL2, column2);
+			}
+		}
+	
+		[Test]
+		public void CSVReaderWriter_WriteThenReadWithOneColumn_ReturnsFailure()
+		{
+			const string VAL1 = "val1";
+
+			using (var csvReaderWriter = new CSVReaderWriter())
+			{
+				csvReaderWriter.Open(testFile, CSVReaderWriter.Mode.Write);
+				csvReaderWriter.Write(VAL1);
+			}
+
+			using (var csvReaderWriter = new CSVReaderWriter())
+			{
+
+				string column1 = null, column2 = null;
+
+				csvReaderWriter.Open(testFile, CSVReaderWriter.Mode.Read);
+				var result = csvReaderWriter.Read(out column1, out column2);
+				Assert.AreEqual(false, result);
+				Assert.AreEqual(VAL1, column1);
+				Assert.AreEqual(null, column2);
+			}
+		}
+
+		[Test]
+		public void CSVReaderWriter_WriteThenReadWithNoColumns_ReturnsFailure()
+		{
+			using (var csvReaderWriter = new CSVReaderWriter())
+			{
+				csvReaderWriter.Open(testFile, CSVReaderWriter.Mode.Write);
+				csvReaderWriter.Write();
+			}
+
+			using (var csvReaderWriter = new CSVReaderWriter())
+			{
+
+				string column1 = null, column2 = null;
+
+				csvReaderWriter.Open(testFile, CSVReaderWriter.Mode.Read);
+				var result = csvReaderWriter.Read(out column1, out column2);
+				Assert.AreEqual(false, result);
+				Assert.AreEqual(null, column1);
+				Assert.AreEqual(null, column2);
+			}
+		}
+	}
+}
---
+++ src/AddressProcessor/CSV/CSVReaderWriter.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.IO;
+
+namespace AddressProcessing.CSV
+{
+    /*
+        2) Refactor this class into clean, elegant, rock-solid & well performing code, without over-engineering.
+           Assume this code is in production and backwards compatibility must be maintained.
+    */
+
+    public class CSVReaderWriter : IDisposable
+    {
+        private StreamReader _readerStream = null;
+        private StreamWriter _writerStream = null;
+
+        [Flags]
+        public enum Mode { Read = 1, Write = 2 };
+
+		public static readonly char Separator = '\t'; 
+
+        public void Open(string fileName, Mode mode)
+        {
+			switch(mode)
+			{
+				case Mode.Read:
+                	_readerStream = File.OpenText(fileName);
+					return;
+
+				case Mode.Write:
+					var fileInfo = new FileInfo(fileName);
+					_writerStream = fileInfo.CreateText();
+					return;
+
+				default:
+					// used to throw Exception but this shouldn't break legacy users
+                	throw new ArgumentOutOfRangeException("Unknown file mode for " + fileName);
+            }
+        }
+
+		// we could write more than 2 columns (but we read only 2); kept for backward compatibility
+        public void Write(params string[] columns)
+        {
+			var outPut = String.Join(Separator.ToString(), columns);
+            _writerStream.WriteLine(outPut);
+        }
+
+		// this method should be parameterless but we're keeping the parameters for backward compatibility
+        public bool Read(string column1 = null, string column2 = null)
+        {
+			return Read(out column1, out column2);
+        }
+
+        public bool Read(out string column1, out string column2)
+        {
+			column1 = null;
+			column2 = null;
+
+			var line = _readerStream.ReadLine();
+			if (string.IsNullOrEmpty(line))
+            {
+                return false;
+            }
+
+            var columns = line.Split(Separator);
+			column1 = columns.Length > 0 ? columns[0] : null;
+            column2 = columns.Length > 1 ? columns[1] : null;
+            return columns.Length > 1;
+        }
+
+        public void Close()
+        {
+            if (_writerStream != null)
+            {
+                _writerStream.Close();
+				_writerStream = null;
+            }
+
+            if (_readerStream != null)
+            {
+                _readerStream.Close();
+				_readerStream = null;
+            }
+        }
+
+		public void Dispose()
+		{
+			Close();
+		}
+    }
+}
